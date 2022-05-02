3c28fe78d5e4e4a5d69e6cade7c68bbb894d9718
Chinmay Chandak <100862990+std447@users.noreply.github.com>
2022-05-02 20:06:01 +0530

    Update startproj.sh

--- startproj.sh
+++ startproj.sh
@@ -21,7 +21,7 @@ tee -a index.html <<HTML
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Document1</title>
   <link rel="stylesheet" href="./assets/css/main.css">
   <script src="./assets/js/main.js"></script>
 </head>
